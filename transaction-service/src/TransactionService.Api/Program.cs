3b15ff17a1c35ea3ce3875fba4d348a1f44897ee
marco.junior <marco.junior@diffs.tech>
2026-03-21 16:44:32 -0300

    asa
    asasa

--- transaction-service/src/TransactionService.Api/Program.cs
+++ transaction-service/src/TransactionService.Api/Program.cs
@@ -1,36 +1,11 @@
-// using Microsoft.Azure.Functions.Worker;
-// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Hosting;
-// using TransactionService.Api.Configuration;
-// using TransactionService.Api.Middlewares;
-// using TransactionService.Api.Security;
-// using TransactionService.Application;
-// using TransactionService.Infrastructure;
-
-// var host = new HostBuilder()
-//     .ConfigureFunctionsWorkerDefaults(worker =>
-//     {
-//         worker.UseMiddleware<ExceptionHandlingMiddleware>();
-//         worker.UseMiddleware<CorrelationIdMiddleware>();
-//         worker.UseMiddleware<IdempotencyKeyMiddleware>();
-//         worker.UseMiddleware<EntraAuthorizationMiddleware>();
-//     })
-//     .ConfigureServices((context, services) =>
-//     {
-//         services.AddApplication();
-//         services.AddInfrastructure(context.Configuration);
-//         services.AddApplicationInsightsTelemetryWorkerService();
-//         services.ConfigureFunctionsApplicationInsights();
-//         services.Configure<EntraAuthorizationOptions>(context.Configuration.GetSection(EntraAuthorizationOptions.SectionName));
-//         services.AddSingleton<EntraAuthorizationEvaluator>();
-//     })
-//     .Build();
-
-// await host.RunAsync();
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TransactionService.Api.Configuration;
 using TransactionService.Api.Middlewares;
+using TransactionService.Api.Security;
+using TransactionService.Application;
+using TransactionService.Infrastructure;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults(worker =>
@@ -38,12 +13,18 @@ var host = new HostBuilder()
         worker.UseMiddleware<ExceptionHandlingMiddleware>();
         worker.UseMiddleware<CorrelationIdMiddleware>();
         worker.UseMiddleware<IdempotencyKeyMiddleware>();
+        worker.UseMiddleware<EntraAuthorizationMiddleware>();
     })
     .ConfigureServices((context, services) =>
     {
+        services.AddApplication();
+        services.AddInfrastructure(context.Configuration);
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.Configure<EntraAuthorizationOptions>(context.Configuration.GetSection(EntraAuthorizationOptions.SectionName));
+        services.AddSingleton<EntraAuthorizationEvaluator>();
     })
     .Build();
 
 await host.RunAsync();
+
