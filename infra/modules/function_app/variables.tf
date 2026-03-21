271ada870394775acf16dc8aaed825b8a15f8d21
marco.junior <marco.junior@diffs.tech>
2026-03-21 15:32:38 -0300

    aa
    aaa

--- infra/modules/function_app/variables.tf
+++ infra/modules/function_app/variables.tf
@@ -3,17 +3,10 @@ variable "resource_group_name" { type = string }
 variable "location" { type = string }
 variable "service_plan_id" { type = string }
 variable "storage_account_name" { type = string }
-variable "storage_account_access_key" { 
-    type = string 
-    sensitive = true 
-    }
-variable "application_insights_connection_string" { 
-    type = string
-    sensitive = true
-  }
+
 variable "key_vault_reference_identity_id" {
-     type = string 
-     default = null 
+     type = string
+     default = null
      }
 variable "app_settings" { type = map(string) }
 variable "auth_settings" {
@@ -25,3 +18,11 @@ variable "auth_settings" {
   })
 }
 variable "tags" { type = map(string) }
+
+variable "project_name" { type = string }
+variable "environment" { type = string}
+
+variable "application_insights_connection_string" {
+  type = string
+  sensitive = true
+}
